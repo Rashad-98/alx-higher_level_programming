0703baca36d3ab20ba436d12f4e1a073961bc499
Rashad-98 <a.rashaad98@gmail.com>
2023-11-21 20:12:02 +0200

    add property to class

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""module with only one class"""
+
+
+class Square:
+    """class that represents a square
+    the class validates the size upon instantiation
+    of an instance
+    """
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
