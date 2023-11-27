696a74ab54bd715491eb347c1596f7f392be9212
Abdelrahman Rashad <a.rashaad98@gmail.com>
2023-11-27 14:12:51 +0200

    add doctest

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 """ This module defines a class
-    that represents a rectangle """
+    that represents a rectangle
+
+    >>> myRectangle = Rectangle()
+    >>>
+"""
 
 
 class Rectangle:
